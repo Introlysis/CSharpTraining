f59cfd9e2d1c9bd5843deb3be82724ce820b51ec
Joseph Senger <jsenger24@gmail.com>
2022-10-09 13:52:06 -0700

    Completes functionality for Start/Classes/Defining/

--- Exercise Files/Start/Classes/Defining/Book.cs
+++ Exercise Files/Start/Classes/Defining/Book.cs
@@ -1,17 +1,28 @@
 using System;
 
-namespace Defining
+namespace Defining2
 {
     // classes have a name, unique within the namespace
     public class Book
     {
         // TODO: classes have member variables, or "fields" to hold data
+        string _name;
+        string _author;
+        int _numPages;
 
 
         // TODO: classes have one or more constructors
+        public Book(string name, string author, int numPages) {
+            _name = name;
+            _author = author;
+            _numPages = numPages;
+        }
 
 
         // TODO: methods are used to operate on the class and data
+        public void Describe() {
+            Console.WriteLine($"{_name} written by {_author} with {_numPages} pages.");
+        }
 
     }
 }
--- Exercise Files/Start/Classes/Defining/Program.cs
+++ Exercise Files/Start/Classes/Defining/Program.cs
@@ -7,9 +7,11 @@ namespace Defining
         static void Main(string[] args)
         {
             // TODO: Create new object instances using the "new" operator
+            Defining2.Book b1 = new Defining2.Book("Test", "Test Author", 5);
 
             
             // TODO: Call a method on the object
+            b1.Describe();
 
 
             // TODO: try to set one of the properties -- 
